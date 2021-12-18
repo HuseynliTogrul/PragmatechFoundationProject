4f283bc81d87d0ab44fef96ef85c16284b90499f
Huseynli Togrul <huseynlitogrul2002@gmail.com>
2021-12-18 14:06:44 +0400

    javascript

---
+++ Works/elave papka/js/action.js
@@ -0,0 +1,36 @@
+let body = document.querySelector("body")
+function changeColor(){
+    body.style.backgroundColor = "lightblue"
+}
+
+
+let h1 = document.querySelector("h1")
+function changeheader(){
+    h1.style.color = "blue"
+}
+
+
+let p = document.querySelector("p")
+let ul = document.querySelector("ul")
+function changemetn(){
+    p.style.color = "green"
+    ul.style.color = "green"
+}
+
+
+let herfler = document.querySelector("p").innerHTML
+function herf(){
+    console.log(herf.length)
+}
+
+
+// let soz = document.querySelectorAll("p").innerHTML
+// function soz(){
+//     console.log(soz.length)
+// }
+
+
+// let sekil = document.querySelector("img").innerHTML
+// function sekil(){
+//     document.querySelector("img").src = "https://res.cloudinary.com/practicaldev/image/fetch/s--UBVTT6xB--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/g83rrbcpsylwwxn9p92q.jpg"
+// }
